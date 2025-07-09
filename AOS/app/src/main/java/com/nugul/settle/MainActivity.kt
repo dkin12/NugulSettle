ecda8e6b8c1dd711405788fed03ef23579395a6d
dkin12 <dkin1020@naver.com>
2025-07-09 11:35:09 +0900

    [이채연] AOS 세팅
    - 안드로이드 프로젝트 세팅
    - google-services.json파일은 제외하고 저장

---
+++ AOS/app/src/main/java/com/nugul/settle/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.nugul.settle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nugul.settle.ui.theme.NugulSettleUpTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NugulSettleUpTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "@String/app_name",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    Greeting(
+                        name = "@String/subTitle",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NugulSettleUpTheme {
+        Greeting("Android")
+    }
+}
